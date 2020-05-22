ceec301964adb5a9247fbb2b9578c1e81f31878c
mbrus <m.brus@stud.elka.pw.edu.pl>
2020-05-22 23:43:05 +0200

    declarations of CarRental's methods

--- src/CarRental/CarRental.hpp
+++ src/CarRental/CarRental.hpp
@@ -5,11 +5,31 @@
 #include "../Customer/Customer.hpp"
 
 
+template <typename T>
+
 class CarRental{
     std::vector <Vehicle> VehicleList;
     std::vector <Customer> CustomerList;
 
 public:
+    CarRental(){
+        VehicleList = new std::vector <Vehicle>();
+        CustomerList = new std::vector <Customer>();
+        
+    }
+    ~CarRental(){
+        delete VehicleList;
+        delete CustomerList;
+    };
+
+    void ReturnVehicleList(/*enum:all, free, taken"*/);
+    void ReturnCustomerList(/*enum: all, withdebt */);
+    void Rent(Vehicle& vehicle);
+    void Payment(Vehicle& vehicle, Customer& customer);
+    CarRental& Search(T& value);
+    void Add(T instance);
+    void Delete(T instance);
+
 
 
 
