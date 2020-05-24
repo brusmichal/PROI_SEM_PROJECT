746c741d9e5f7b7cdd6cec4b284894bc79ec2281
Łukasz Tumialis <l.tumialis@stud.elka.pw.edu.pl>
2020-05-25 00:25:01 +0200

    Update Vehicle.hpp

--- src/Vehicle/Vehicle.hpp
+++ src/Vehicle/Vehicle.hpp
@@ -1,9 +1,24 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
 #include "../VehicleRental/VehicleRental.hpp"
+#include <string>
 
 
-class Vehicle : protected VehicleRental <Vehicle>{
+class Vehicle : protected VehicleRental <Vehicle> {
+	int dateProduction;
+	string numberplate;
+	int costOfRenting;
+	int condition;
+	bool isRent;
+	bool isWork;
 
+	Vehicle(int, string, int);
+	~Vehicle();
+
+	bool canRent();
+	void Rent();
+	void Return();
+	void ReduceCondition();
+	void Repair();
 };
 #endif
