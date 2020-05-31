5705afb6f56d3d15942c461bca3d800349f4d4d9
Łukasz Tumialis <l.tumialis@stud.elka.pw.edu.pl>
2020-05-31 22:38:29 +0200

    Update Vehicle.hpp

--- src/Vehicle/Vehicle.hpp
+++ src/Vehicle/Vehicle.hpp
@@ -1,10 +1,11 @@
 #ifndef VEHICLE_H
 #define VEHICLE_H
+#include <iostream>
 #include "../VehicleRental/VehicleRental.hpp"
 #include <string>
 
-
-class Vehicle : protected VehicleRental{
+class Vehicle /*: protected VehicleRental <Vehicle>*/ {
+protected:
 	std::string name;
 	int dateProduction;
 	std::string numberplate;
@@ -13,15 +14,36 @@ class Vehicle : protected VehicleRental{
 	bool isRent;
 	bool isWork;
 
+public:
 	Vehicle(std::string, int, std::string, int);
-	~Vehicle();
-
+	Vehicle(std::string, int, std::string, int, int, bool, bool);
+	Vehicle();
+	//	~Vehicle();
+protected:
 	bool canRent();
 	void Rent();
 	void Return();
 	void ReduceCondition();
 	void Repair();
 
+	friend
+		std::ostream& operator<< (std::ostream& os, Vehicle& vehi);
+
 	friend class Customer;
+	friend class VehicleRental;
+};
+
+class Car :protected Vehicle {
+	int numberOfSeats;
+
+	Car(std::string, int, std::string, int, int);
+	//	~Car();
+};
+
+class Truck :protected Vehicle {
+	int capacity;
+
+	Truck(std::string, int, std::string, int, int);
+	//	~Truck();
 };
 #endif
