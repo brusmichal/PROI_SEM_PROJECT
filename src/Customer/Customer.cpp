6bf0d6af0eb290a13ef27815e77a09a02216758e
Rafał Bednarz <r.bednarz@stud.elka.pw.edu.pl>
2020-05-31 23:26:01 +0200

    edit  return

--- src/Customer/Customer.cpp
+++ src/Customer/Customer.cpp
@@ -13,6 +13,15 @@ Customer::Customer(const std::string str1, const std::string str2, const long lo
 	debt = deb;
 }
 
+Customer::Customer(const std::string str1, const std::string str2, const long long pes, const char type)
+{
+	name = str1;
+	surname = str2;
+	pesel = pes;
+	driving_license_type = type;
+	rented_vehicle = Vehicle("", 0, "BS00000", 0);
+	debt = 0;
+}
 
 void Customer::rent_vehicle(const Vehicle car)
 {
@@ -32,8 +41,10 @@ void Customer::rent_vehicle(const Vehicle car)
 
 void Customer::return_vehicle()
 {
-
-	Customer::rented_vehicle = {};
+	if (rented_vehicle.name != "")
+		rented_vehicle = Vehicle();
+	else
+		throw "Osoba nie posiada wypozyczonego pojazdu";
 }
 
 void Customer::pay(const int price)
@@ -44,12 +55,18 @@ void Customer::pay(const int price)
 void Customer::show_history()
 {
 	std::cout << "Historia wypożyczeń:" << std::endl;
-	//for (int i = 0; i < Customer::history.size(); ++i)
-		//cout << Customer::history[i]<<endl;
+	for (int i = 0; i < Customer::history.size(); ++i)
+		cout << Customer::history[i]<<endl;
 }
+
+char Customer::show_driving_type()
+{
+	return driving_license_type;
+}
+
 
 std::ostream& operator<< (std::ostream& os, Customer& klient)
 {
 	return os << klient.name + " " + klient.surname + " Pesel: " + std::to_string(klient.pesel)
-		+ " Zaleglosc: " + std::to_string(klient.debt)<<" Wypo\276yczone: "<<klient.borrowed.name<<std::endl;
+		+ " Zaleglosc: " + std::to_string(klient.debt)<<" Wypo\276yczone: "<<klient.borrowed.numberplate<<std::endl;
 }
--- src/Customer/Customer.hpp
+++ src/Customer/Customer.hpp
@@ -14,17 +14,20 @@ class Customer : protected VehicleRental{
 	char driving_license_type;
 	Vehicle rented_vehicle;
 	std::vector<Vehicle> history;
-	int debt=0;
+	int debt;
 	
     public:
 		Customer(const std::string, const std::string, const long long, const char, Vehicle, int); //Konstruktor
+		Customer(const std::string, const std::string, const long long, const char);
 		void rent_vehicle(const Vehicle); //funkcja do wypozyczania pojazdu
 		void pay(const int); //funkcja oplaty
 		void return_vehicle(); //funkcja do zwracania pojazdu
 		void show_history();
+		char show_driving_type(); //zwraca typ prawojazd
 
 	friend
 		std::ostream& operator<< (std::ostream& os, Customer& cust); //operator wyswietlenia osoby
     friend class Vehicle;
+	friend class VehicleRental;
 };
 #endif
