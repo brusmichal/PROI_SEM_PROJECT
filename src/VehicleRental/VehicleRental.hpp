53334cb6313740dcbd90fd5df17586cb81cf6a00
Łukasz Tumialis <l.tumialis@stud.elka.pw.edu.pl>
2020-06-03 22:58:33 +0200

    Update VehicleRental.hpp

--- src/VehicleRental/VehicleRental.hpp
+++ src/VehicleRental/VehicleRental.hpp
@@ -5,31 +5,32 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include "../Vehicle/Vehicle.hpp"
+//#include "../Vehicle/Vehicle.hpp"
 #include "../Customer/Customer.hpp"
 
-std::ifstream in;
-std::ofstream out;
 
-
-class VehicleRental{
-    std::vector <Vehicle> VehicleList;
+class VehicleRental {
+public:
+    std::vector <Car> CarList;
+    std::vector <Truck> TruckList;
     std::vector <Customer> CustomerList;
 
 public:
-    VehicleRental(){
-        VehicleList = new std::vector <Vehicle>();
-        CustomerList = new std::vector <Customer>();
-        
+    VehicleRental() {
+        std::vector <Car>* CarList = new std::vector <Car>();
+        std::vector <Truck>* TruckList = new std::vector <Truck>();
+        std::vector <Customer>* CustomerList = new std::vector <Customer>();
+
     }
-    ~VehicleRental(){
-        delete VehicleList;
+    /*~VehicleRental() {
+        delete CarList;
+        delete TruckList;
         delete CustomerList;
-    };
+    };*/
 
     void Menu();
     void menu_customer(int);
-    void menu_vehicle(int);
+    void menu_vehicle(int, bool);
     void LoadData();
     void ExportData();
 
@@ -37,12 +38,12 @@ public:
     void Payment(Customer& customer, int money);
     void Return(Customer& customer);
 
-    template <typename T>
-    typename std::vector<T>::iterator Find(const T& value);
+    //template <typename T>
+    //typename std::vector<T>::iterator Find(const T& value);
     template <typename T>
     void Add(T& instance);
-    template <typename T>
-    void Delete(T& instance);
+    //template <typename T>
+    //void Delete(T& instance);
 
     // void ShowVehicleList(/*enum:all, free, taken, car, truck"*/);
     // void ShowCustomerList(/*enum: all, withdebt */);
@@ -50,8 +51,8 @@ public:
 
 
 
-template <typename T>
-friend void ShowInfo(T& instance);
-    
+//template <typename T>
+//friend void ShowInfo(T& instance);
+
 };
 #endif
