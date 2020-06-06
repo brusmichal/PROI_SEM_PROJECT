f171afdfdace39c107ac68f9bc0fbbc48639626f
Łukasz Tumialis <l.tumialis@stud.elka.pw.edu.pl>
2020-06-06 14:26:22 +0200

    Add Pause() in IO

--- src/IO_functions/IO_functions.hpp
+++ src/IO_functions/IO_functions.hpp
@@ -1,7 +1,15 @@
 #ifndef IO_FUNCTIONS_H
 #define IO_FUNCTIONS_H
 
-void pause(){};
+
+void pause() {
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::string dummy;
+    std::cout << "Press any key to continue . . .";
+    std::getline(std::cin, dummy);
+}
+
 void clear(){};
 
 #endif
